c4947f109a8848c82e3c798c99cd010a7be8753d
sam <sammcmillan97@gmail.com>
2025-11-26 10:27:23 +1300

    Merge sorted array

---
+++ Array/Easy/MergeSortedArray.java
@@ -0,0 +1,20 @@
+//88. Merge Sorted Array
+package Array.Easy;
+
+public class MergeSortedArray {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i = m - 1;
+        int j = n - 1;
+        int k = n + m -1;
+        while(j != -1) {
+            if (i == - 1 || nums1[i] < nums2[j]) {
+                nums1[k] = nums2[j];
+                j--;
+            } else {
+                nums1[k] = nums1[i];
+                i--;
+            }
+            k--;
+        }
+    }
+}
