eaee679f7b042eec46062ac469886d9b83e9108f
sam <sammcmillan97@gmail.com>
2026-08-31 09:53:08 +1200

    392. Is Subsequence

---
+++ String/Easy/IsSubsequence.java
@@ -0,0 +1,27 @@
+package String.Easy;
+
+public class IsSubsequence {
+    public boolean isSubsequence(String s, String t) {
+        int currentLetter = 0;
+        int i = 0;
+
+        if (s.length() > t.length()) {
+            return false;https://leetcode.com/problems/is-subsequence/
+        }
+
+        if (s.length() == 0) {
+            return true;
+        }
+
+        while(i < t.length()) {
+            if (currentLetter >= s.length()) {
+                return true;
+            }
+            if(s.charAt(currentLetter) == t.charAt(i)) {
+                currentLetter++;
+            }
+            i++;
+        }
+        return false;
+    }
+}
